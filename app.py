f82a0b6120fe0a5f6372845c72de654fef1deaa2
haya1010 <73729974+haya1010@users.noreply.github.com>
2021-12-17 19:28:10 +0900

    'modified-error'

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def update_flag(key, new_value):
     db.session.commit()
 
 def delete_flag(key):
-    flag_list = db.session.query(Flag).filter(Flag.key == key).all()
+    flag_list = db.session.query(Flag).filter(Flag.key == key).one()
     db.session.delete(flag_list)
     db.session.commit()
 
